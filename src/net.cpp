c0afb1868b22c9e7ae9dc233a42f13d0be2c0d87
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-08-16 15:01:44 +0900

    Added msg command to profiler

--- src/net.cpp
+++ src/net.cpp
@@ -2801,6 +2801,7 @@ void CConnman::PushMessage(CNode* pnode, CSerializedNetMsg&& msg)
     size_t nMessageSize = msg.data.size();
     size_t nTotalSize = nMessageSize + CMessageHeader::HEADER_SIZE;
     PROFBR("PushMessage");
+    PROFBR(msg.command);
     BitcoinProfiler::UsedBandwidth(nTotalSize);
     LogPrint(BCLog::NET, "sending %s (%d bytes) peer=%d\n",  SanitizeString(msg.command.c_str()), nMessageSize, pnode->GetId());
 
