0756775baafcb4d06c9bb6499754e1a58335d5d3
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-08-28 17:22:07 +0900

    Sync profiler every 10 min instead of every min

--- src/util.cpp
+++ src/util.cpp
@@ -1123,7 +1123,7 @@ void BitcoinProfilerLoad() {
 void BitcoinProfilerSynchronize() {
     static int64_t lastSync = 0;
     int64_t now = GetTime();
-    if (lastSync + 60 < now) {
+    if (lastSync + 600 < now) {
         {
             LOCK(m_bp);
             printf("BitcoinProfilerSynchronize()\n");
